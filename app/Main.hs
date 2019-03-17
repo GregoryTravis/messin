0801408fc67de3570cab9567d18a84d05ba96081
Gregory Travis <greg.m.travis@gmail.com>
2019-03-17 11:17:12 -0400

    Mut

--- app/Main.hs
+++ app/Main.hs
@@ -1,6 +1,7 @@
 module Main where
 
 import Control.Exception (try)
+import qualified Data.Map.Strict as M
 import System.IO (getLine, putStrLn)
 import System.IO.Error (isEOFError)
 
@@ -15,7 +16,32 @@ muts = do
       let [cmd, arg] = words line
       return $ Just (cmd, arg)
 
+newtype Mut s a = Mut { mutStep :: s -> (a, s) }
+composeMuts :: Mut s a -> Mut s b -> Mut s b
+composeMuts Mut{ mutStep = a } Mut{ mutStep = b } = Mut { mutStep = c }
+  where c s = case a s of (x, s') -> b s
+composeMutsV :: Mut s a -> (a -> Mut s b) -> Mut s b
+composeMutsV Mut{ mutStep = a } f = Mut { mutStep = c }
+  where c s = case a s of (x, s') -> case f x of Mut{ mutStep = b } -> b s'
+setMut :: Ord k => k -> v -> Mut (M.Map k v) ()
+setMut k v = Mut { mutStep = \m -> ((), M.insert k v m) }
+getMut :: Ord k => k -> Mut (M.Map k v) v
+getMut k = Mut { mutStep = \m -> (m M.! k, m) }
+runMut :: Mut s a -> s -> (a, s)
+runMut Mut{ mutStep  = mutStep } = mutStep
+
+incMut :: (Ord k, Num v) => k -> Mut (M.Map k v) ()
+incMut k = composeMutsV (getMut k) (\n -> setMut k (n + 1))
+
 main :: IO ()
 main = do
+  let m :: M.Map String Int
+      m = M.empty
+  let ((), m') = runMut (setMut "a" 5) m
+  msp m
+  msp m'
+  let (5, _) = runMut (getMut "a") m'
   x <- muts
+  let ((), m'') = runMut (incMut "a") m'
+  msp m''
   msp x

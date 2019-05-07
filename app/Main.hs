7e6fc69d1559c3c8caecb9259d890593258d46fa
Gregory Travis <greg.travis@listenfirstmedia.com>
2019-05-07 14:15:31 -0400

    Read db and oops

--- app/Main.hs
+++ app/Main.hs
@@ -1,5 +1,13 @@
 module Main where
 
 import Mut 
+import Util 
+
+data DB = DB { a :: Int }
+  deriving (Read, Show)
+
 main = do
-  monadly
+  --monadly
+  s <- readFile "history.db"
+  let db = (read s :: DB)
+  msp db
